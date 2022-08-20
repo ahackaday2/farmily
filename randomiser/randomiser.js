338f40358929cdeab40e4c5de87df47d5e28ec80
e0425705 <puahxiuwen@gmail.com>
2022-08-21 00:12:35 +0800

    skeleton layout

---
+++ randomiser/randomiser.js
@@ -0,0 +1,15 @@
+var isQuote = false;
+var isJoke = false;
+var isTip = false;
+
+function getQuotes() {
+    isQuote = true;
+}
+
+function getJokes() {
+    isJoke = true;
+}
+
+function getTips() {
+    isTip = true;
+}
