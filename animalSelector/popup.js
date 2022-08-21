eda851cdb4f9e3ec243fc637a9f94e9229f94551
franceneee <57038559+franceneee@users.noreply.github.com>
2022-08-21 13:25:47 +0800

    Move files into folder

--- animalSelector/popup.js
+++ animalSelector/popup.js
@@ -6,11 +6,11 @@ window.onload = function () {
     const forwardButton = document.getElementById("forward");
     const nextButton = document.getElementById("next");
     const animalImage = document.getElementById("animal");
-    const selectButton = document.getElementById("selectButton");
+    const submitButton = document.getElementById("submitButton");
     const container = document.getElementById("animalSelector");
+    const form = document.getElementById('animalForm');
 
     nextButton.addEventListener("click", () => {
-        console.log("next");
         if (i + 1 == 3) {
             i = 0;
         } else {
@@ -21,7 +21,7 @@ window.onload = function () {
     });
 
     forwardButton.addEventListener("click", () => {
-        console.log("forward");
+        console.log("forward")
         if (i - 1 == -1) {
             i = 2;
         } else {
@@ -31,16 +31,15 @@ window.onload = function () {
         animalImage.src = "./images/" + animals[i] + ".png";
     });
 
-    selectButton.addEventListener("click", () => {
-        nextButton.style.display = "none";
+    submitButton.addEventListener("click", () => {
+        /* nextButton.style.display = "none";
         forwardButton.style.display = "none";
-        selectButton.style.display = "none";
-
+        submitButton.style.display = "none";
+ */
         chrome.storage.sync.set({ animal: animals[i] });
         chrome.storage.sync.get("animal", (result) => { console.log(result)});
         let div = document.createElement("div");
-        div.innerHTML = "You have selected " + animals[i];
+        //div.innerHTML = "You have selected " + animals[i];
         container.appendChild(div);
     });
 };
-
